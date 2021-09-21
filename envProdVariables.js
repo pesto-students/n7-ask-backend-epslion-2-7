fe6d14078c8b70e5e04058434bcaf450d0676202
Nikhil Kumar <nikhil@vayana.com>
2021-09-21 08:56:44 +0530

    Ask-23 - Added env variables

---
+++ envProdVariables.js
@@ -0,0 +1,9 @@
+{
+
+  "SECRET_KEY": "Ask",
+  "RDS_DB": "Ask",
+  "RDS_PORT": "3306",
+  "RDS_USERNAME": process.,
+  "RDS_PASSWORD": "admin007",
+  "RDS_HOSTNAME": "ask-1.cthdmx0yvc9b.ap-south-1.rds.amazonaws.com"
+}
